1c6005213bf51e8efbc95d9c005bfc5223d3f7c8
Olena-Nechyporenko <127235568+Olena-Nechyporenko@users.noreply.github.com>
2023-09-29 19:13:22 +0300

    Fixed errors

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,26 +1,23 @@
-import css from "./FriendList.module.css"
-import clsx from "clsx"
+import css from './FriendList.module.css';
+import clsx from 'clsx';
 
-export const FriendList = ({children}) => {
-    return (
-        <ul className={css.friendlist}>
-        {children}
-        </ul>
-    )
-}
-
-export const FriendListItem = ({friends}) => {
-    return (
-       friends.map(({avatar, name, isOnline, id}) => 
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendlist}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
         <li key={id} className={css.item}>
-        <span className={clsx(
-            css.status, 
-            isOnline ? css.online : css.offline)}
-        >
-        </span>
-        <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-        <p className={css.name}>{name}</p>
-      </li>
-        )    
-    )
-}
+          <span
+            className={clsx(css.status, isOnline ? css.online : css.offline)}
+          ></span>
+          <img
+            className={css.avatar}
+            src={avatar}
+            alt="User avatar"
+            width="48"
+          />
+          <p className={css.name}>{name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,23 +1,25 @@
-import css from "./Statistics.module.css"
+import css from './Statistics.module.css';
 
-export const Statistics = ({title, stats}) => {
-    return (
-        <section className={css.statistics}>
-      {title && (<h2 className={css.title}>{title}</h2>)}
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
 
-    <ul className={css.statlist}>
-    {stats.map(({id, label, percentage}) => 
-    
-     <li style={{backgroundColor: getRandomHexColor()}} key={id} className={css.item}>
-      <span className={css.label}>{label}</span>
-      <span className={css.percentage}>{percentage}%</span>
-    </li>
-   
-    )}
-    </ul>
-</section>
-    )
-}
+      <ul className={css.statlist}>
+        {stats.map(({ id, label, percentage }) => (
+          <li
+            style={{ backgroundColor: getRandomHexColor() }}
+            key={id}
+            className={css.item}
+          >
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
